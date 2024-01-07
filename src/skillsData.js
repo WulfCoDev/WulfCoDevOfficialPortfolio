16c5f0b539ef5725b862feb3312f95965de4617f
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2024-01-07 04:41:02 +0000

    created skills array

---
+++ src/skillsData.js
@@ -0,0 +1,37 @@
+import { DiHtml5 } from "react-icons/di";
+
+
+export const Skills = [
+    {skillName: 'HTML',
+     skillIcon: <DiHtml5 />,
+},
+{skillName: 'CSS',
+     skillIcon: '',
+},
+{skillName: 'Javascript',
+     skillIcon: '',
+},
+{skillName: 'React',
+     skillIcon: '',
+},
+{skillName: 'Node.js',
+     skillIcon: '',
+},
+{skillName: 'Express.js',
+     skillIcon: '',
+},
+{skillName: 'MongoDB',
+     skillIcon: '',
+},
+{skillName: 'Tailwind CSS',
+     skillIcon: '',
+},
+{skillName: 'REST API',
+     skillIcon: '',
+},
+{skillName: 'Version Control',
+     skillIcon: '',
+},
+
+
+]
