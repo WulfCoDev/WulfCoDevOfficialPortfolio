b8ede36f9ec2b88fdf31ad8e1d0bfa49dffc1da8
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2024-01-06 16:52:43 -0800

    links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,22 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
 
+    const navigate = useNavigate();
 
     return (
         <section id="contact" className="container mx-auto h-screen w-screen flex justify-center items-center">
             <div className="">
                 <h1 className="font-primary text-green-500 animate-bounce text-6xl">Lets Collaborate</h1>
                 <div className="mt-2 text-6xl *:cursor-pointer text-green-400 flex justify-center items-center space-x-6">
-                <button className="">
+                <a href="https://github.com/WulfCoDev" target="_blank" className="">
                   <FaGithub />  
-                </button>
+                </a>
+                <a href="https://www.linkedin.com/in/dev-christian-alexander/" target="_blank">
+                   <FaLinkedin /> 
+                </a>
                 
-                <FaLinkedin />
                 </div>
             </div>
         </section>
