50bc62f9441903d1210ecb5d898bd39b4d6915fd
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2024-01-08 21:12:06 -0800

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,13 @@ const About = () => {
     return (
         <section id="about" className="container mx-auto h-screen w-screen flex justify-center items-center">
             <div className="flex justify-center items-center">
-                About
+                <div className="w-[500px]">
+                   <h1 className="font-primary text-green-400 text-2xl mb-6">About Me</h1>
+                    <p className="font-tertiary text-white font-bold text-[24px] mb-6">I'm a Front-end Developer with over a decade of graphic design experience</p>
+                    <p className="font-tertiary text-white text-[18px] mb-6">I am a Web Developer with a background in IT/Tech repair and have over a decade of graphic design experience. I am seeking remote opportunities as a Web Developer or Software Engineer.</p>
+                    <p className="font-tertiary text-white text-[18px] mb-6">What drew me to web development is the ability to express my creativity whilst leveraging my design skills from my graphic design experience. On the other hand, web development succumbs my need for problem-solving and constant self-evolution; growth.</p>
+                    <p className="font-tertiary text-white text-[18px] mb-6">In my free time, you can find me sim-racing or just hanging out with friends on Discord. I also enjoy making 3D prints and other things such as custom stickers, clothes, etc.</p>
+                </div>
             </div>
         </section>
     )
